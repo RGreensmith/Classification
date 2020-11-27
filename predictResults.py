24e2c74642b92f4dc8c7076e8e9bb0c05c3ab1ec
RGreensmith <rose.greensmith@yahoo.co.uk>
2020-11-27 16:13:15 +0000

    Adding documentation to predictResults function

--- predictResults.py
+++ predictResults.py
@@ -3,6 +3,21 @@ import numpy as np
 from predictClassifier import predictClassifier
 
 def predictResults(xTest, yTest, model, modelTypeFull):
+
+    """
+    Predicts new data based on x test data, and saves as csv in current directory.
+
+    Args:
+
+    xTest (pandas dataframe, required): x test data
+    yTest (pandas dataframe, required): y test data
+    model (model object, required): classifier model object of sklearn
+    modelTypeFull (string, required): name of the type of model
+
+    Returns:
+    `yPred`: prediction based on x test data
+    """
+
     yPred = predictClassifier(model, xTest)
     yPred_pd = pd.DataFrame(yPred)
     yPred_pd.to_csv('y_prediction_'+modelTypeFull+'.csv')
